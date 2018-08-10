ff3d0864240ae62ba49fef7774bfee4a998e07d1
Cassindi <tara.devin.b@gmail.com>
2018-08-10 13:04:25 -0500

    html and some js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,28 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var friends = [];
+
+app.get("/api/friends", function(req,res){
+
+    return res.json(friends);
+
+});
+
+app.post("/api/friends", function(req,res){
+
+    var newUser = "";
+
+    friends.push(newUser);
+
+    res.json(newUser);
+
+});
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/survey", function(req,res){
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
