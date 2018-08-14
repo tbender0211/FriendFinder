9306782df30baa9c0ff09c79da7a5a3c033f5d55
Cassindi <tara.devin.b@gmail.com>
2018-08-13 19:57:09 -0500

    friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,5 @@
 
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
+var friends = require("../data/friends");
 
 
 module.exports = function(app){
