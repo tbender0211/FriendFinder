dcea97fb4856f1e9be0d193461d77ba8accb46a6
Cassindi <tara.devin.b@gmail.com>
2018-08-17 12:05:49 -0500

    working again!

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,10 @@
 var friends = [
     {
         name: "Sylvanas",
-        pic: "",
-        q1: 3,
-        q2: 4,
-        q3: 5,
-        q4: 2,
-        q5: 3,
-        q6: 1,
-        q7: 4,
-        q8: 4,
-        q9: 3,
-        q10: 1
+        pic: "https://d1u5p3l4wpay3k.cloudfront.net/wowpedia/thumb/5/51/SylvanasCinematic.png/300px-SylvanasCinematic.png?version=2417e89194b3c06f0f88b6f5f353cd3c",
+        answers: [3, 4, 5, 2, 3, 1, 4, 4, 3, 1],
+        total: 30,
+        difference: 0
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,18 @@ module.exports = function(app){
 
     });
 
+    // app.get("/api/friends", function(data){
+
+    //     for(i=0; i < friends.length; i++){
+            
+    //         var match = getClosest.number(newUser.total, friends[i].total);
+    
+    //         console.log(match);
+    
+    //     }
+    
+    // });
+
     app.post("/api/friends", function(req,res){
 
         friends.push(req.body);
